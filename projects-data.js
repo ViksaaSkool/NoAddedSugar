a37e26e906c9615403a69462a29a8daa778e7119
ViksaaSkool <victor.arsovski@gmail.com>
2026-04-14 15:24:15 +0200

    redesign, add project tnc and pp

--- projects-data.js
+++ projects-data.js
@@ -1,26 +1,25 @@
 window.PROJECTS = [
-  // Add new projects by copying this shape:
-  // {
-  //   title: "example.com",
-  //   image: "asset/projects/example.png",
-  //   fit: "contain",
-  //   position: "center center",
-  //   description: "Short project description here.",
-  // },
-  {
-    title: "brajko.cloud",
-    image: "asset/projects/brajko_cloud.png",
-    fit: "contain",
-    position: "center center",
-    description:
-      "A cloud-native product identity with a strong character, simple flow, and a presentation style that feels more like a persona than a landing page.",
-  },
   {
     title: "pythonin90sec.com",
     image: "asset/projects/python_in_90sec.png",
     fit: "contain",
     position: "center center",
     description:
-      "Fast learning, compact delivery, and a playful interface around short-form Python teaching. Built to feel direct, useful, and a little loud.",
+      "A practical joke in time when there was over-production and over-sell of Python courses.\n<a href=\"https://www.pythonin90sec.com//\">I want to learn Python in 90 seconds</a>",
   },
+  {
+    title: "Grace App",
+    image: "asset/projects/grace_app.png",
+    fit: "contain",
+    position: "center center",
+    description: "A mobile app for <a href=\"https://blessameal.com/\">blessing a meal</a>. It has been done as part of a master thesis research and you can read more about it <a href=\"https://is.gd/duUnmY\">here</a>, or check out the open source code <a href=\"https://github.com/ViksaaSkool/Grace\">here</a>",
+  },
+  {
+    title: "brajko.cloud",
+    image: "asset/projects/brajko_cloud.png",
+    fit: "contain",
+    position: "center center",
+    description:
+      "Soundboard of Brajko, one of the aliases used by <a href=\"https://en.wikipedia.org/wiki/Yugoslavia\">ex-yu</a> region <a href=\"https://www.tiktok.com/@dragoslavstanoje8\">most famous</a> as TikToker. In the Balkans, Tiktok doesn't support creators program, so the goal is to put the Tiktok-to-web page revenue route to the test. Tiktok users from a viral account are enocouraged to visit the soundboard page, act that is monitizable. Want to see the soundboard?\nHere: <a href=\"https://brajko.cloud\">brajko.cloud</a>",
+  }  
 ];
